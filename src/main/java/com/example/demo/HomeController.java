e057ff7f6c35fa6434bab4d969525c5fd5ceeb77
Fir3eye <93431222+Fir3eye@users.noreply.github.com>
2024-08-04 18:07:11 +0530

    Create HomeController.java

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.sendevops;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome To Sen DevOps");
+        return "home";
+    }
+}
