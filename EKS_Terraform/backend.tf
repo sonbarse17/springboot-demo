c989f54fd8f2137d4015160cdfb1766c882f1fcf
Fir3eye <93431222+Fir3eye@users.noreply.github.com>
2024-08-26 20:10:01 +0530

    Update backend.tf

--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sen-devops2525" # Replace with your actual S3 bucket name
+    bucket = "sen-dev34343" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
